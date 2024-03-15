b51bfdaf1ebe2a20ac8dadf83a9250f245850ae7
pizzahutdude333 <128006576+pizzahutdude333@users.noreply.github.com>
2024-03-14 18:27:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+text= "";
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("text").innerHTML = text;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+  
+}
